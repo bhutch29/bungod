f3a1724fee0b19e06d2ceeadb4bb890c828c8e94
Bradley Hutchinson <bradley.hutchinson@keysight.com>
2025-02-27 23:27:06 -0800

    define release

--- mix.exs
+++ mix.exs
@@ -9,7 +9,13 @@ defmodule Bungod.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      releases: [
+        bungod: [
+          include_executables_for: [:unix],
+          applications: [runtime_tools: :permanent]
+        ]
+      ],
     ]
   end
 
