1204400b53e3bf437a0e35f0f181e6512b57cee0
Bradley Hutchinson <bradley.hutchinson@keysight.com>
2025-10-02 15:21:42 -0700

    Revert "attempt to fix rebroadcasting clipboard issues"
    This reverts commit b75fbacbbf807fd52ccc3389cb2cf46b45ef5329.

--- lib/bungod/clipboard_server.ex
+++ lib/bungod/clipboard_server.ex
@@ -9,7 +9,7 @@ defmodule Bungod.ClipboardServer do
   def init(_) do
     Phoenix.PubSub.subscribe(Bungod.PubSub, "clipboard")
     schedule_check()
-    {:ok, %{content: Clipboard.paste()}}
+    {:ok, %{content: Clipboard.paste(), syncing: false}}
   end
 
   # Clipboard uses Port which sends :closed message that needed handling to avoid crash
@@ -20,22 +20,20 @@ defmodule Bungod.ClipboardServer do
   def handle_info({:update_clipboard, clipboard}, state) do
     Logger.info("New clip: #{clipboard}")
     Clipboard.copy(clipboard)
-    {:noreply, %{state | content: clipboard}}
+    {:noreply, %{state | content: clipboard, syncing: true}}
   end
 
-  def handle_info(:check_clipboard, %{content: content} = state) do
+  def handle_info(:check_clipboard, %{content: content, syncing: syncing} = state) do
     clipboard = Clipboard.paste()
 
-    new_state = if clipboard != nil && clipboard != "" && clipboard != content do
-      Logger.debug("Clipboard contents changed locally, broadcasting.")
+    if clipboard != nil && clipboard != "" && clipboard != content && !syncing do
+      Logger.debug("Clipboard contents changed, broadcasting.")
       Phoenix.PubSub.broadcast(Bungod.PubSub, "clipboard", {:update_clipboard, clipboard})
-      %{state | content: clipboard}
-    else
-      state
     end
 
     schedule_check()
-    {:noreply, new_state}
+
+    {:noreply, %{state | content: clipboard, syncing: false}}
   end
 
   defp schedule_check do
